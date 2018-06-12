5e20bb287a0a5717d416eecee391acb39d792638
stark24423 <39294554+stark24423@users.noreply.github.com>
2018-06-12 15:51:09 +0800

    Add files via upload
    analog to digital

---
+++ adc.ino
@@ -0,0 +1,32 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(A0,INPUT);
+  pinMode(1,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+   pinMode(10,OUTPUT);
+      
+  
+
+}
+
+void loop() {
+  int k = analogRead(A0);
+
+      for(int i =1;i<=9;i++)
+      {
+        if(k==(i-1))
+        digitalWrite(i,1);
+        else
+        digitalWrite(i,0);  
+        
+        }
+        Serial.println(analogRead(A0)); 
+
+}
